6078e101e5f79be13c117da0f11b3f901e8509c6
Miloslav Jezek <milo.jezek@gmail.com>
2024-03-25 23:16:07 +0100

    Use azure storage string and create test function for listing containers

--- src/services/azure_storage.py
+++ src/services/azure_storage.py
@@ -1,9 +1,9 @@
 import logging
-import os
 
 from azure.core.exceptions import ResourceNotFoundError
-from azure.storage.blob import BlobClient
+from azure.storage.blob import BlobClient, BlobServiceClient
 
+from .azure_storage_config import AZURE_STORAGE_CONNECTION_STRING
 
 def get_blob_as_bytes(container_name: str, blob_name: str) -> bytes:
     """
@@ -12,7 +12,7 @@ def get_blob_as_bytes(container_name: str, blob_name: str) -> bytes:
     :raises ValueError: If `AZURE_STORAGE_CONNECTION_STRING` environment variable is not set.
     :raises NameError: If the requested file was not found on Azure Storage.
     """
-    conn_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    conn_str = AZURE_STORAGE_CONNECTION_STRING
     if not conn_str:
         raise ValueError("`AZURE_STORAGE_CONNECTION_STRING` environment variable is not set.")
 
@@ -24,3 +24,16 @@ def get_blob_as_bytes(container_name: str, blob_name: str) -> bytes:
         logging.exception(f"File {container_name}/{blob_name} not found.")
         raise NameError(f"Scan not found. Requested file ({blob_name}) was not found on Azure Storage.") from err
 
+
+def list_containers() -> list[str]:
+    """
+    List all containers in the Azure Storage account.
+
+    :raises ValueError: If `AZURE_STORAGE_CONNECTION_STRING` environment variable is not set.
+    """
+    conn_str = AZURE_STORAGE_CONNECTION_STRING
+    if not conn_str:
+        raise ValueError("`AZURE_STORAGE_CONNECTION_STRING` environment variable is not set.")
+
+    blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+    return [container.name for container in blob_service_client.list_containers()]
