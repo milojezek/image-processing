dfef66f98d8f20f9216b156fb790c3ac60231097
Miloslav Jezek <milo.jezek@gmail.com>
2024-03-25 17:00:38 +0100

    Use db connection string in run_db_statement function in  db.py

--- src/services/db.py
+++ src/services/db.py
@@ -1,13 +1,12 @@
-import os
-
 import psycopg
 from psycopg.rows import Row
+from .db_config import DB_CONNECTION
 
 
 def run_db_statement(statement: str) -> list[Row]:
-    conn_str = os.getenv("DB_CONNECTION_STRING")
+    conn_str = DB_CONNECTION
     if not conn_str:
-        raise ValueError("`DB_CONNECTION_STRING` environment variable is not set.")
+        raise ValueError("'DB_CONNECTION' environment variable is not set.")
 
     # Connect to a database
     with psycopg.connect(conn_str) as conn:
